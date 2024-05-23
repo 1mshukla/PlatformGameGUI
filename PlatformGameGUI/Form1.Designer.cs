4764e04b84e6300e8792f00c56a9a2ab8b5225f5
1mshukla <1mshukla@A6-WSB4-B360>
2024-05-23 14:02:47 +1200

    Add project files.

---
+++ PlatformGameGUI/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PlatformGameGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.player = new System.Windows.Forms.PictureBox();
+            this.screen = new System.Windows.Forms.Panel();
+            this.block = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.block)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.player.Location = new System.Drawing.Point(321, 205);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(76, 55);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // screen
+            // 
+            this.screen.Controls.Add(this.block);
+            this.screen.Controls.Add(this.player);
+            this.screen.Location = new System.Drawing.Point(0, 0);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(800, 438);
+            this.screen.TabIndex = 1;
+            // 
+            // block
+            // 
+            this.block.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.block.Location = new System.Drawing.Point(486, 288);
+            this.block.Name = "block";
+            this.block.Size = new System.Drawing.Size(136, 147);
+            this.block.TabIndex = 1;
+            this.block.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 433);
+            this.Controls.Add(this.screen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.screen.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.block)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Panel screen;
+        private System.Windows.Forms.PictureBox block;
+    }
+}
+
---
+++ PlatformGameGUI/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlatformGameGUI
+{
+    public partial class Form1 : Form
+    {
+        //declare constants
+        const int G = 30;
+        //declare variables
+        bool right;
+        bool left;
+        bool up;
+        bool down;
+        bool jump;
+        bool fall;
+        int force;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //if ( player.Right > block.Left  && player.Right <= block.Right && player.Bottom > block.Top)
+            //{
+            //    right = false;
+            //}
+            //if (player.Left < block.Right && player.Left > block.Left && player.Bottom > block.Top)
+            //{
+            //    left = false;
+            //}
+            if (player.Bounds.IntersectsWith(block.Bounds))
+            {
+                right = false;
+            }
+            if (player.Left < block.Right && player.Left > block.Left && player.Bottom > block.Top)
+            {
+                left = false;
+            }
+
+            //if (player.Right > block.Left && player.Right < block.Right && player.Bottom > block.Top)
+            //{
+            //    block.Top = player.Bottom;
+            //    jump = false;
+            //
+            //}
+
+            //if (player.Right > block.Left && player.Right < block.Right && player.Bounds.IntersectsWith(block.Bounds))
+            //{
+            //    block.Top = player.Bottom;
+            //    jump = false;
+            //
+            //}
+
+
+            if (right == true)
+            {
+                player.Left += 5;
+
+            }
+            else if (left == true)
+            {
+                player.Left -= 5;
+            }
+            else if (up == true)
+            {
+                player.Top -= 5;
+            }
+            else if (down == true)
+            {
+                player.Top += 5;
+            }
+
+            if (jump == true)
+            {
+                player.Top -= force;
+                force -= 1;
+                
+            }
+            
+            if (player.Right < block.Left || player.Right > block.Right && jump == true)
+            {
+                
+                if (player.Bottom >= screen.Height)
+                {
+                    player.Top = screen.Height - player.Height;
+                    jump = false;
+                }
+            }
+            
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                    right = true;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                left = true;
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                up = true;
+
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                down = true;
+
+            }
+            if (jump != true)
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    jump = true;
+                    force = G;
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                right = false;
+
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                left = false;
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                up = false;
+
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                down = false;
+
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+    }
+}
